b1392caa892b281315c0a8b90d528d881d1ce8aa
Margokomilfo-dev <margokomilfo@mail.ru>
2020-10-28 22:38:56 +0100

    Updates

---
+++ precache-manifest.0ce6e741f6d9243824c52c225c1bee45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1c2ded551c2424394941b4acab28dd",
+    "url": "/exam-tuesday/index.html"
+  },
+  {
+    "revision": "870aa50c9973749b9752",
+    "url": "/exam-tuesday/static/css/main.0f4087f6.chunk.css"
+  },
+  {
+    "revision": "c32c91d3fba68faa8eae",
+    "url": "/exam-tuesday/static/js/2.187690c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exam-tuesday/static/js/2.187690c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "870aa50c9973749b9752",
+    "url": "/exam-tuesday/static/js/main.c387c810.chunk.js"
+  },
+  {
+    "revision": "6613614be367abf6bf75",
+    "url": "/exam-tuesday/static/js/runtime-main.52947114.js"
+  }
+]);
